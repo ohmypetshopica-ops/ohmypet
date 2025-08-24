a7ea7d9b538d34292346917aaaa068f5ce64d2ad
Codearlo <flavio.cabanah@gmail.com>
2025-08-24 15:44:08 -0500

    a

--- frontend/supabase-client.js
+++ frontend/supabase-client.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Tus credenciales de Supabase
-const supabaseUrl = 'https://pzpjpjfpulkrfcdviyte.supabase.co'; // <-- Reemplaza esto
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6cGpwamZwdWxrcmZjZHZpeXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNTcyOTUsImV4cCI6MjA3MDkzMzI5NX0.nFyEqoWNgySC5g6INbwi2rPDVNda48xgygWJVmPIn60';       // <-- Reemplaza esto
+const supabaseUrl = 'https://pzpjpjfpulkrfcdviyte.supabase.co'; // <-- Reemplaza este
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6cGpwamZwdWxrcmZjZHZpeXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNTcyOTUsImV4cCI6MjA3MDkzMzI5NX0.nFyEqoWNgySC5g6INbwi2rPDVNda48xgygWJVmPIn60';       // <-- Reemplaza este
 
 // Crear y exportar el cliente de Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
