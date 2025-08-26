a7c2690789792b149ec8309bb371e838796649b3
Codearlo <flavio.cabanah@gmail.com>
2025-08-25 22:59:57 -0500

    delete files

--- frontend/js/models/citas.model.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from '../../supabase-client.js';
-export async function getCitasProgramadas() {
-  const { data, error } = await supabase
-    .from('citas').select('id').gte('fecha', new Date().toISOString());
-  if (error) throw error;
-  return data?.length ?? 0;
-}
